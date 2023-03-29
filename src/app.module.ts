4f72df137fb5342ca00afd78c6f0f512bcdb2957
Ann-tech <Onyekaann17@gmail.com>
2023-03-29 22:14:59 +0100

    create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './typeorm/entities/user.entity';
 
-
+ConfigModule.forRoot()
 
 @Module({
   imports: [
@@ -11,10 +13,10 @@ import { AppService } from './app.service';
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: '',
-      password: '',
-      database: '',
-      entities: [],
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      entities: [User],
       synchronize: true,
     })
   ],
