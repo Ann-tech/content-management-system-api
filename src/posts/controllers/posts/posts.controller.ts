624e732d5876f382a37ec64569623961bf9be151
Ann-tech <Onyekaann17@gmail.com>
2023-04-03 13:23:11 +0100

    add get all posts functionality

--- src/posts/controllers/posts/posts.controller.ts
+++ src/posts/controllers/posts/posts.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import { PostsService } from 'src/posts/services/posts/posts.service';
 
 @Controller('/api/v1/posts')
 export class PostsController {
     constructor(private readonly postsService: PostsService) {}
+
+    @Get()
+    public async getPosts(@Request() request: Request) {
+        return this.postsService.getPosts()
+    }
 }
--- src/posts/services/posts/posts.service.ts
+++ src/posts/services/posts/posts.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { POSTEntity } from 'src/typeorm/entities/post.entity';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+    constructor(
+        @InjectRepository(POSTEntity)
+        private readonly postsRepo: Repository<POSTEntity>, ) {}
+    async getPosts() {
+        return await this.postsRepo.find({}) 
+    }
+}
