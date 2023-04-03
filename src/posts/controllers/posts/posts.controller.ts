1ba29ef6748a93ebdb9151146e9ff9fa9ec80887
Ann-tech <Onyekaann17@gmail.com>
2023-04-03 13:47:30 +0100

    protect create post route using guards

--- src/posts/controllers/posts/posts.controller.ts
+++ src/posts/controllers/posts/posts.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Request } from '@nestjs/common';
+import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { PostsService } from 'src/posts/services/posts/posts.service';
 
 @Controller('/api/v1/posts')
@@ -8,5 +9,11 @@ export class PostsController {
     @Get()
     public async getPosts(@Request() request: Request) {
         return this.postsService.getPosts()
+    }
+
+    @Post() 
+    @UseGuards(AuthGuard())
+    public async createPosts() {
+
     }
 }
